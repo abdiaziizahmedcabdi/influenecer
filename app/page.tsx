3df21f4ef4e3feab1bcf4915332ef6192372d606
abdiaziiz <abdiaziizahmedcabdi>
2026-03-09 15:01:07 +0300

    Restore Platform Showcase component

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import { Navbar } from "@/components/sections/Navbar";
 import { Hero } from "@/components/sections/Hero";
+import { PlatformShowcase } from "@/components/sections/PlatformShowcase";
 import { DemoBooking } from "@/components/sections/DemoBooking";
 import { TrustReassurance } from "@/components/sections/TrustReassurance";
 import { BackupContact } from "@/components/sections/BackupContact";
@@ -11,6 +12,7 @@ export default function Home() {
       <Navbar />
       <main>
         <Hero />
+        <PlatformShowcase />
         <DemoBooking />
         <TrustReassurance />
         <BackupContact />
